f2f472b99e4a584e6abd99f37665e037935427c9
Christopher <chris.krtr@gmail.com>
2019-12-13 22:26:59 -0800

    let it snow

--- src/app/models/snow.ts
+++ src/app/models/snow.ts
@@ -15,11 +15,58 @@ export namespace Color {
   type blue = number; // 0 - 255
   type opacity = number; // 0 - 1
   export type RGBA = [red, green, blue, opacity? ];
+  export type Hex = string;
 
-  type HexByte  = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | 'a' | 'b' | 'c' | 'd' | 'e' | 'f';
-  export type Hex      = '#' & HexByte & HexByte & HexByte & HexByte & HexByte & HexByte;
-  export type HexAlpha = '#' & HexByte & HexByte & HexByte & HexByte & HexByte & HexByte & HexByte & HexByte;
+}
 
+class Color {
+  rgb: Color.RGBA;
+  hex: Color.Hex;
+  constructor(color: Color.Hex | Color.RGBA) {
+    try {
+      Color.assertHex(color)
+      this.rgb = [parseInt(color.slice(1, 3) as string, 16),
+                  parseInt(color.slice(3, 5) as string, 16),
+        parseInt(color.slice(5, 7) as string, 16)];
+      this.hex = color as Color.Hex
+
+    } catch {
+      Color.assertRGB(color);
+      this.rgb = color as Color.RGBA;
+      const toHex = (i: number) => i.toString(16).padStart(2, '0');
+      this.hex = `#${toHex(this.rgb[0])}${toHex(this.rgb[1])}${toHex(this.rgb[2])}` + color[3] ? toHex(this.rgb[3]) : ''
+    }
+  }
+  static hex(color: Color.Hex): Color {
+    Color.assertHex(color);
+
+    return new Color(color);
+  }
+
+  static assertHex(color: any): void { // asserts color is Color.Hex {
+    Color.assert(color.match(/^#([0-9a-f]{3}|[0-9a-f]{6})$/i), 'Color is not a valid Hex color')
+  }
+
+  static assertRGB(color: any): void { // asserts color is Color.RGBA {
+    const [r, g, b] = color;
+    Color.assert(
+      ((r >= 0 && r <= 255) &&
+      (g >= 0 && g <= 255) &&
+      (b >= 0 && b <= 255)),
+      'Provided color is not a valid rgb color'
+    )
+  }
+
+  private static assert(condition: any, msg?: string): void { // asserts condition {
+    if (!condition) {
+      throw new Error(msg || 'Bad Color Value)');
+    }
+  }
+
+  static rgba(...rgb: Color.RGBA): Color {
+    Color.assertRGB(rgb);
+    return new Color(rgb);
+  }
 }
 
 
@@ -30,7 +77,7 @@ export interface Snow {
   zRange: Range;
   windowWidth: number; // if the worker can determine this, then don't pass it.
   windowHeight: number;
-  color: Color.RGBA | Color.HexAlpha | Color.Hex
+  color: Color.RGBA | Color.Hex
   maxFlakeStems: number; //  use a value < 2 for a circle. The higher this value, the more rendering time required per flake
   minFlakeStems: number; //  use a value < 2 to allow simple circle flakes
 }
--- src/app/snow.worker.ts
+++ src/app/snow.worker.ts
@@ -1,19 +1,25 @@
 /// <reference lib="webworker" />
 
 import { Snow } from 'src/app/models/snow';
+
 interface SnowEvent extends MessageEvent {
   data: Snow
 }
 
+const flakes = []
+
+const generateFlake = (config) => {
+  return [];
+}
+
 addEventListener('message', ({ data }: SnowEvent) => {
   const response = `worker response to ${data.baseSize}`;
+  const flake = generateFlake(data)
   postMessage(response);
 });
 
-const generateFlake = () => {
-
-}
 
 // Javascript is single threaded, meaning that every operation happens one after the other. this includes UI updates. Webworkers allows another 'thread' to be opened to do work at the same time the main thread is running. This is called parallelism and the only way to do it in the web.
 
 // Does this explanation make any sense?
+

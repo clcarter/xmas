1615404bd27a6598bbdf26d0f553391cf32483bf
Christopher <chris.krtr@gmail.com>
2020-11-28 20:20:39 -0800

    remove sensitive info

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: 'AIzaSyCw2HNCWbTRFimouqPLMuWaNSNj-zGfaBg',
-    authDomain: 'xmas-84941.firebaseapp.com',
-    databaseURL: 'https://xmas-84941.firebaseio.com',
-    projectId: 'xmas-84941',
-    storageBucket: 'xmas-84941.appspot.com',
-    messagingSenderId: '1083138495696'
-  }
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,6 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyCw2HNCWbTRFimouqPLMuWaNSNj-zGfaBg',
-    authDomain: 'xmas-84941.firebaseapp.com',
-    databaseURL: 'https://xmas-84941.firebaseio.com',
-    projectId: 'xmas-84941',
-    storageBucket: 'xmas-84941.appspot.com',
-    messagingSenderId: '1083138495696'
-  }
 };
 
 /*
